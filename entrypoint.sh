3c45167902ef26e46a4664e2de3a686ef0476c54
chathuranga836@gmail.com <chaturanga836@gmail.com>
2025-11-17 18:00:29 +0530

    remove db script

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,12 +10,12 @@ until pg_isready -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER"; do
 done
 
 # Initialize DB once
-if [ ! -f ${RANGER_HOME}/.db_initialized ]; then
-    echo "--- Initializing Ranger Database: Running db_setup.py ---" # Added confirmation
-    cd ${RANGER_HOME}/db
-    python3 db_setup.py install || exit 1
-    touch ${RANGER_HOME}/.db_initialized
-fi
+# if [ ! -f ${RANGER_HOME}/.db_initialized ]; then
+#     echo "--- Initializing Ranger Database: Running db_setup.py ---" # Added confirmation
+#     cd ${RANGER_HOME}/db
+#     python3 db_setup.py install || exit 1
+#     touch ${RANGER_HOME}/.db_initialized
+# fi
 
 echo "Starting Ranger Admin service..."
 cd ${RANGER_HOME}/ews
